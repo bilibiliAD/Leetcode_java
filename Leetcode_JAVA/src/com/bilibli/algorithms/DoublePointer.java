7a59eeaf7960eed0d7c486181874ee64adee42f7
惊梦、 <bilibiliad@icloud.com>
2018-08-27 17:36:36 +0800

    day14

--- Leetcode_JAVA/src/com/bilibli/algorithms/DoublePointer.java
+++ Leetcode_JAVA/src/com/bilibli/algorithms/DoublePointer.java
@@ -8,9 +8,9 @@ public class DoublePointer {
     /*
      * Input: numbers={2, 7, 11, 15}, target=9
      *从一个已经排序的数组中查找出两个数，使它们的和为 0
-      *  Two Sum II - Input array is sorted (Easy)
-      *  day 13  2018年8月24日 10:54:43
-       * */
+     *  Two Sum II - Input array is sorted (Easy)
+     *  day 13  2018年8月24日 10:54:43
+     * */
     public int[] twoSum(int[] numbers, int target) {
         int i = 0;
         int j = numbers.length - 1;
@@ -27,6 +27,27 @@ public class DoublePointer {
             }
         }
         return null;
+    }
+
+
+    /*
+    * Sum of Square Numbers (Easy)
+    * 判断一个数是否为两个数的平方和，例如 5 = 12 + 22。
+    * day 14  2018年8月27日 16:46:44
+    * */
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+        while (left <= right) {
+            int powSum  = left * left + right * right;
+            if (powSum  == c) {
+                return true;
+            } else if (powSum  > c) {
+                right--;
+            } else {
+                left++;
+            }
+        }
     }
 
 }
